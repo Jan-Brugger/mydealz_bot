753885cda1d82ad702a7556d6c9a0f57b17c478e
Jan Brugger <jan.brugger@fti.de>
2021-12-27 12:13:21 +0100

    put feed-parser in own thread

--- daemon.py
+++ daemon.py
@@ -1,27 +1,31 @@
 import asyncio
+import threading
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from pytz import utc
 
 from src.bot import Bot
 from src.core import Core
 from src.parser import Parser
 
-if __name__ == '__main__':
-    Core.init()
 
-    scheduler = AsyncIOScheduler()
-    job = scheduler.add_job(
+def parse_job() -> None:
+    loop = asyncio.new_event_loop()
+    app_scheduler = AsyncIOScheduler(event_loop=loop, timezone='Europe/Berlin')
+    app_scheduler.add_job(
         Parser.parse,
         'interval',
         seconds=60,
-        timezone=utc,
         max_instances=50
     )
-    scheduler.start()
-    Bot().run()
+    app_scheduler.start()
 
     try:
-        asyncio.get_event_loop().run_forever()
+        loop.run_forever()
     except (KeyboardInterrupt, SystemExit):
         pass
+
+
+if __name__ == '__main__':
+    Core.init()
+    threading.Thread(target=parse_job).start()
+    Bot().run()
